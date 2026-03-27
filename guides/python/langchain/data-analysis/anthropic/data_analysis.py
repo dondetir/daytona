5932d8d8f09e735af3d61bef9a9adb2062428a08
Muhammad Hashmi <mhashmi@berkeley.edu>
2026-03-27 00:51:54 +0000

    fix(examples,guides): sync python import-order parity
    Signed-off-by: Muhammad Hashmi <mhashmi@berkeley.edu>

--- guides/python/langchain/data-analysis/anthropic/data_analysis.py
+++ guides/python/langchain/data-analysis/anthropic/data_analysis.py
@@ -4,13 +4,14 @@
 """LangChain data analysis example using Daytona sandboxes."""
 import base64
 
-from daytona import ExecutionArtifacts
 from dotenv import load_dotenv
 from langchain.agents import create_agent  # pylint: disable=import-error
 from langchain_anthropic import ChatAnthropic  # pylint: disable=import-error
 
 # pylint: disable=import-error
 from langchain_daytona_data_analysis import DaytonaDataAnalysisTool
+
+from daytona import ExecutionArtifacts
 
 load_dotenv()
 
